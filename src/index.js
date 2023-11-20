765e7ebfed84ad0278e5087ee6808986cde1efa2
Amjad Hussain <malikamjad3300@gmail.com>
2023-11-20 11:50:13 +0500

    model jwt and bcrypt hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import dotenv from "dotenv"
+import express from "express"
+const app=express()
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants"
 import connectDB from "./db/connectedDB.js";
 
 dotenv.config({path:'./env'})
+
+
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000,()=>{
+        console.log(`Server Listening At ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("DB connection Failed:" ,err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from "express"
