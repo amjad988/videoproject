9d8327f4807d3e4963593b137c1c0796abe8c363
Amjad Hussain <malikamjad3300@gmail.com>
2023-11-24 12:40:56 +0500

    add usercontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,72 @@
 import { asyncHandler } from "../utiles/ayncHandler.js";
-
-
+import {ApiError} from "../utiles/ApiError.js"
+import { User } from "../models/user.model.js";
+import {uploaudOnCloudinary} from "../utiles/cloudnary.js"
+import { ApiResponse } from "../utiles/ApiRes.js";
 const registerUser=asyncHandler(async(req,res)=>{
-      res.status(200).json({
-        message:"Bawa G Chass A Gi"
-    })
-})
+     const {fullname,email,username,password}=req.body 
+     console.log("email:",email);
+
+     if([fullname,email,username,password].some((field)=>field?.trim()===""))
+      {
+        throw new ApiError(400,"All fields are required")
+      }
+
+      const existedUser= User.findOne(
+        {
+          $or:[{ username },{ email }]
+        }
+      )
+      if(existedUser)
+      {
+        throw new ApiError(409,"User with email or Username alredy existed")
+      }
+
+     const avatarLocalPath= req.files?.avatar[0]?.path;
+     const coverImageLocalPath= req.files?.coverImage[0]?.path
+    
+     if(!avatarLocalPath)
+     {
+      throw new ApiError(400,"Avatar file is required")
+     }
+
+    const avatar= await uploaudOnCloudinary(avatarLocalPath)
+    const coverImage=await uploaudOnCloudinary(coverImageLocalPath)
+    
+    if(!avatar)
+    {
+      throw new ApiError(400,"Avatar file Required")
+    }
+  
+    const user=await User.create(
+      {
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+      }
+    )
+
+    const createdUser=await User.findById(user._id)
+    .select("-password -refreshToken")
+
+    if(!createdUser)
+    {
+      throw new ApiError(500,"Something went wrong while Register a user")
+    }
+
+    return res.status(201).json(
+      new ApiResponse(200,createdUser,"User Register Succeed")
+    )
+    
+  })
+    
+    
+    
+    
+
 
 const errorChaker=asyncHandler(async(req,res)=>{
   res.status(200).json({
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser,errorChaker } from "../controllers/user.controller.js";
 const router=Router()
+import {upload} from "../middlewares/multer.middleware.js"
+
 
 router.route("/error").get(errorChaker)
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields(
+        [
+            {
+                name:"avatar",
+                maxCount:1
+            },
+            {
+                name:"coverImage",
+                maxCount:1
+            }
+        ]
+    ),
+    registerUser)
 
 export default router
